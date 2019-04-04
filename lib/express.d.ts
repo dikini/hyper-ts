b068b56c54e1172ac6663beea33c6a34483c5b24
gcanti <giulio.canti@gmail.com>
2019-04-04 12:28:46 +0200

    add lib folder

---
+++ lib/express.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="node" />
+import { Request, RequestHandler, ErrorRequestHandler, Response } from 'express';
+import { IncomingMessage } from 'http';
+import { Connection, CookieOptions, HeadersOpen, Middleware, ResponseEnded, Status } from '.';
+export declare type Action = {
+    type: 'setBody';
+    body: unknown;
+} | {
+    type: 'endResponse';
+} | {
+    type: 'setStatus';
+    status: Status;
+} | {
+    type: 'setHeader';
+    name: string;
+    value: string;
+} | {
+    type: 'clearCookie';
+    name: string;
+    options: CookieOptions;
+} | {
+    type: 'setCookie';
+    name: string;
+    value: string;
+    options: CookieOptions;
+};
+export declare class ExpressConnection<S> implements Connection<S> {
+    readonly req: Request;
+    readonly res: Response;
+    readonly actions: Array<Action>;
+    readonly _S: S;
+    constructor(req: Request, res: Response, actions?: Array<Action>);
+    chain<T>(action: Action): ExpressConnection<T>;
+    getRequest(): IncomingMessage;
+    getBody(): unknown;
+    getHeader(name: string): unknown;
+    getParams(): unknown;
+    getQuery(): unknown;
+    getOriginalUrl(): string;
+    getMethod(): string;
+    setCookie(name: string, value: string, options: CookieOptions): ExpressConnection<HeadersOpen>;
+    clearCookie(name: string, options: CookieOptions): ExpressConnection<HeadersOpen>;
+    setHeader(name: string, value: string): ExpressConnection<HeadersOpen>;
+    setStatus(status: Status): ExpressConnection<HeadersOpen>;
+    setBody(body: unknown): ExpressConnection<ResponseEnded>;
+    endResponse(): ExpressConnection<ResponseEnded>;
+}
+export declare function toRequestHandler<I, O, L>(middleware: Middleware<I, O, L, void>): RequestHandler;
+export declare function toErrorRequestHandler<I, O, L>(f: (err: unknown) => Middleware<I, O, L, void>): ErrorRequestHandler;
+export declare function fromRequestHandler<I, A>(requestHandler: RequestHandler, f: (req: Request) => A): Middleware<I, I, never, A>;
